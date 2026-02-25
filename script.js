b3226a4e95ba9c381ff5d3e7c2dd44c6c3bd3299
Sebastian Ramirez <sebas14737@gmail.com>
2026-02-25 09:07:13 -0500

    Se continua cargando el script

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 function pedirDatos() { // pide datos al usuario y los devuelve en un array
 
-let usuario = prompt("Ingrese su usuario:");
-let contraseña = prompt("Ingrese su contraseña:");
+let usuario = prompt("Ingrese su usuario:")
+let contraseña = prompt("Ingrese su contraseña:")
 
-console.log("Usuario:", usuario);
-console.log("Contraseña:", contraseña);
+console.log("Usuario:", usuario)
+console.log("Contraseña:", contraseña)
 
-return [usuario, contraseña];
+return [usuario, contraseña]
 
 }
 
 function verificarDatos(usuario, contraseña) {   // verifica si los datos ingresados son correctos
-    
-let usuarioCorrecto = "admin";
-let contraseñaCorrecta = "1234";
+
+let usuarioCorrecto = "admin"
+let contraseñaCorrecta = "1234"
 
 if(usuario === usuarioCorrecto && contraseña === contraseñaCorrecta){
 
-return true;
+return true
 
 }else{
 
-return false;
+return false
 
 }
+
+}
+function login(){
+
+let intentos = 0;
+
+while(intentos < 3){
+
+let datos = pedirDatos();
+
+let usuario = datos[0];
+let contraseña = datos[1];
+
+let acceso = verificarDatos(usuario, contraseña);
+
+if(acceso){
+
+alert("Acceso permitido");
+return;
+
+}else{
+
+alert("Datos incorrectos");
+intentos++;
+
+}
+
+}
+
+alert("Usuario bloqueado");
 
 }
